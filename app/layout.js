0bf87dd39a41bf0d2b1d2182ec649d6fe35ecb0f
mgusmano <mgusmano@yahoo.com>
2024-03-31 22:08:24 -0400

    new

--- app/layout.js
+++ app/layout.js
@@ -6,10 +6,30 @@ import Nav from './layout/Nav';
 import Footer from './layout/Footer';
 
 export default function RootLayout({ children }) {
+  var ReExtData = {
+    "sdkversion": "7.7.0",
+    "toolkit": "classic",
+    "theme": "classic",
+    "packages": {
+      "charts": false,
+      "fontawesome": false,
+      "ux": false,
+      "calendar": false,
+      "d3": false,
+      "exporter": false,
+      "pivot": false,
+      "pivotd3": false,
+      "pivotlocale": false,
+      "froalaeditor": false
+    },
+    "debug": false,
+    "license": "b116e32e-0847-4764-94ef-a16987070407",
+    "url": "local"
+  }
   return (
     <html className="zcol" lang="en">
       <body className="zcol">
-        <ReExtProvider>
+        <ReExtProvider ReExtData={ReExtData}>
           <Header />
           <div className="zrow">
             <Nav />
